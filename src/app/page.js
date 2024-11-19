d82f28b92bb3b875f8dbd9ff2d81b4edcda8b150
sherlockdong <sherlockdong2007@gmail.com>
2024-11-19 18:09:55 -0500

    ddes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.title}>PHO - GUIDE</div>
-
+    <div className={styles.desp}>Designed by Sherlock Dong</div>
     </div>
   );
 }
