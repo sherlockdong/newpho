1338eabf20d86ff64111d02fa487396e97e48d42
Yakun Dong <sherlockdong2007@gmail.com>
2025-11-06 22:27:42 -0500

    jessie

--- src/app/aboutus/page.jsx
+++ src/app/aboutus/page.jsx
@@ -9,6 +9,7 @@ const Count = () => {
 
 <Link href='https://sherlockdong.us' target="_blank" rel="noopener noreferrer"><p id="linkcolor">Sherlock's personal website</p></Link>
 </div>
+<div id="highschool"><p>Yajie Yu is the founder of Pho-Guide, a learning platform dedicated to making advanced physics competition concepts accessible through clarity and creativity. She is an IB Diploma student at Nanjing Foreign Language School specializing in physics, mathematics, and astrophysics. Yajie combines research experience in antenna design, cosmology, and fluid mechanics with a passion for visual learning and scientific communication.</p></div>
 </div>
       </>
     );
